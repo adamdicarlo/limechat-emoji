3d127b646bb52b83583b0f8869cabdf60ffbdd78
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2012-12-15 19:25:43 -0800

    Fix regex bug, simplify code, use emoji-cheat-sheet.com's github repo emojis.
    The regex had an unescaped - in the character class caused a ton of extra
    characters to be accepted, as it was interpreted as a range.

--- Limelight-emoji.js
+++ Limelight-emoji.js
@@ -1,23 +1,10 @@
-String.prototype.startsWith = function (s) {
-    return this.indexOf(s) == 0;
-}
-
-Element.prototype.empty = function() {
-    while (this.firstChild) this.removeChild(this.firstChild);
-}
-
-document.addEventListener("DOMNodeInserted", function(e) {
-    var line = e.target;
-    if (line.getAttribute('type') == 'privmsg') {
-        var imgbase = 'https://raw.github.com/tmitz/rails_emoji/master/vendor/assets/images/emojis/';
-        var msg = line.getElementsByClassName('message').item(0);
-
-        var html = msg.innerHTML;
-        while (emoji = html.match(/:([\d\w+-_]+):/)) {
-            var url = imgbase + emoji[1] + '.png';
-            var icon = '<span class="emoji" style="background-image: url(' + url + ')"></span>';
-            html = html.replace(':' + emoji[1] + ':', icon);
-        }
-        msg.innerHTML = html;
-    }
+document.addEventListener('DOMNodeInserted', function(e) {
+  var line = e.target;
+  if (line.getAttribute('type') == 'privmsg') {
+    var replace = '<span class="emoji" style="background-image: url(\'' +
+        'https://raw.github.com/arvida/emoji-cheat-sheet.com/master/public' +
+        '/graphics/emojis/$1.png\')"></span>';
+    var msg = line.getElementsByClassName('message').item(0);
+    msg.innerHTML = msg.innerHTML.replace(/:([\w\d+\-_]+):/g, replace);
+  }
 }, false);
